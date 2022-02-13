01bb4afbd121e998ef1f1bb65d293a0f5c423687
Даниель Городецкий <daniel_gorodezki@outlook.com>
2022-02-13 16:32:45 +0600

    Add procent status

--- MatrixTest/MatrixTest.h
+++ MatrixTest/MatrixTest.h
@@ -4,8 +4,12 @@
 #include <vector>
 #include "Martix.h"
 #include <cmath>
+#include <Windows.h>
+#include <chrono>
+#include <conio.h>
 class Matrix;
 vector<Matrix> Vec;
 using namespace std;
 int min_I = 0;
 bool erase_foo(vector<int>& vec, int a);
+void print_vector(std::vector<int> const& input);
--- MatrixTest/Source.cpp
+++ MatrixTest/Source.cpp
@@ -2,11 +2,15 @@
 int main()
 {
 	setlocale(LC_ALL, "RU");
+	auto start = chrono::high_resolution_clock::now();
 	int size = 11;
-	int N = pow(2, ((size - 2) * 2)) - 1;
+	unsigned int N = pow(2, ((size - 2) * 2)) - 1;
+	unsigned int procent100 = N / 100;
+	unsigned int procent = N - procent100;
+	int proc = 1;
 	int M = (size - 2) * 2;
 	int M2 = size - 2;
-	for (N; N >= 0; N--)
+	for (N; N > 0; N--)
 	{
 		vector<int> X = { 0,1,2,3,4,5,6,7,8,9,10 };
 		vector<int> Y = { 0,1,2,3,4,5,6,7,8,9,10 };
@@ -20,12 +24,25 @@ int main()
 				}
 			}
 		}
+		if (N == procent)
+		{
+			system("cls");
+			cout << proc << " %" << endl;
+			proc++;
+			procent -= procent100;
+		}
 		Matrix(X, Y).check_matrix(false);
+		//print_vector(X);
+		//print_vector(Y);
 	}
-	cout << Vec.size() << endl;
-	cout << min_I << endl;
+	system("cls");
+	auto end = chrono::high_resolution_clock::now();
+	chrono::duration<float> duration = end - start;
+	cout << "Time = " << duration.count() << "s" << endl;
+	cout << "Total - " << Vec.size() << endl;
+	cout << "Index min_K - " << min_I << endl;
 	Vec[min_I].print_matrix();
-
+	_getch();
 }
 bool erase_foo(vector<int> &vec, int a)
 {
@@ -40,4 +57,11 @@ bool erase_foo(vector<int> &vec, int a)
 	advance(it, a);
 	vec.erase(it);
 	return true;
+}
+void print_vector(std::vector<int> const& input)
+{
+	for (int i = 0; i < input.size(); i++) {
+		std::cout << input.at(i) << ' ';
+	}
+	std::cout << std::endl << std::endl;
 }
